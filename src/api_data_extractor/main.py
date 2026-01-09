70ebdc8474ca202afd912490bb3769e28ab07d6e
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2026-01-09 11:09:24 -0600

    Fetch weather data

--- src/api_data_extractor/main.py
+++ src/api_data_extractor/main.py
@@ -3,3 +3,20 @@ main.py
 
 Entry point for the API Data Extractor project.
 """
+from weather_api import WeatherAPI
+
+def main():
+    """
+    Main entry point for the calculator application.
+    """
+    weatherdata = WeatherAPI(
+        access_key="94e876191d384feb0e64597adf2635f6",
+        base_url="http://api.weatherstack.com/current",
+        location="New York"
+    )
+
+    data = weatherdata.get_current_weather()
+    print(data)
+
+if __name__ == "__main__":
+    main()
